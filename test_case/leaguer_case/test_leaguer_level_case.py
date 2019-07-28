6368096a538462d97593c4d38561af8e8e0fbe18
ty <346315670@qq.com>
2019-07-28 16:10:40 +0800

    初始版

---
+++ test_case/leaguer_case/test_leaguer_level_case.py
@@ -0,0 +1,44 @@
+# coding:utf-8
+from test_case.base_case import Base_Case
+from po.pages.leaguer_menu.leaguer_level_page import Leaguer_Level_Page
+import unittest
+
+test_data = {'级别名称': 'atuo级别', '续期价格': 10, '补卡价格': 10, '积分过期时长': 2, '注销退款系数': 1,
+             '换卡价格': 2, '备注': '备注1', '预期结果': '保存成功'}
+
+
+# @unittest.skip("跳过")
+class Leaguer_Level_Case(Base_Case):
+
+    # @unittest.skip("跳过")
+    def test_001_leaguer_level_add(self):
+        super().default_login()
+        leaguer_level_page = Leaguer_Level_Page(self.base, test_data)
+        leaguer_level_page.switch_in_leaguer_level_menu()
+        leaguer_level_page.click_add_btn()
+        leaguer_level_page.input_add_info()
+        leaguer_level_page.click_save_btn()
+        leaguer_level_page.assert_add_result()
+
+    def test_002_leaguer_level_query(self):
+        super().default_login()
+        leaguer_level_page = Leaguer_Level_Page(self.base, test_data)
+        leaguer_level_page.switch_in_leaguer_level_menu()
+        leaguer_level_page.input_query_level_name()
+        leaguer_level_page.click_query_btn()
+        leaguer_level_page.assert_query_result()
+
+    def test_003_leaguer_level_del(self):
+        super().default_login()
+        leaguer_level_page = Leaguer_Level_Page(self.base, test_data)
+        leaguer_level_page.switch_in_leaguer_level_menu()
+        leaguer_level_page.input_query_level_name()
+        leaguer_level_page.click_query_btn()
+        leaguer_level_page.click_query_checkbox()
+        leaguer_level_page.click_del_btn()
+        leaguer_level_page.click_del_is_okbtn()
+        leaguer_level_page.assert_del_result()
+
+
+if __name__ == '__main__':
+    unittest.main()
