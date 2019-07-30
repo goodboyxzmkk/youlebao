a61728030acc937f7c55a8c1a66e2d8e3879061b
ty <346315670@qq.com>
2019-07-30 09:31:34 +0800

    修改日志方法，只保留10天的日志

--- common/logger.py
+++ common/logger.py
@@ -7,7 +7,7 @@ class Logger():
     def __init__(self):
         '''指定保存日志的文件路径，日志级别，以及调用文件，将日志存入到指定的文件中'''
         # 文件名
-        self.logName = os.path.join(config_manage.LOG_PATH, '%s.log' % time.strftime('%Y-%m-%d'))
+        self.logName = os.path.join(config_manage.LOG_PATH, '%s.log' % time.strftime('%Y-%m-%d'))  # 以年月日做为文件名
         # 创建logger
         self.logger = logging.getLogger('YCH')
         # 设置日志等级
@@ -42,15 +42,16 @@ class Logger():
         # 避免日志输出重复问题
         self.logger.removeHandler(ch)
         self.logger.removeHandler(fh)
-
+        # 删除多余日志
         self.__del_log()
 
     def __del_log(self):
         '''只保留10天的日志'''
-        dirs_list = os.listdir(config_manage.LOG_PATH)  # 获取路径下所有文件
-        file_path = os.path.join(config_manage.LOG_PATH, dirs_list[0])  # 列表中的第一个文件
-        if len(dirs_list) > 10:
+        dirs_list = os.listdir(config_manage.LOG_PATH)  # 获取路径下所有日志文件
+        while len(dirs_list) > 10:
+            file_path = os.path.join(config_manage.LOG_PATH, dirs_list[0])  # 列表中的第一个文件
             os.remove(file_path)
+            dirs_list = os.listdir(config_manage.LOG_PATH)
 
     def debug(self, msg):
         self.__console('debug', msg)
